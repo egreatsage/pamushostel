97805937e852f904d63823a978c6f852309d7f26
egreatsage <michaelcull9715@gmail.com>
2023-05-28 13:44:53 -0700

    fc

--- src/client/Components/Navbar.jsx
+++ src/client/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {AiOutlineMenu} from 'react-icons/ai'
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -8,37 +9,40 @@ const Navbar = () => {
   };
 
   return (
-    <div className='fixed top-0 left-0 right-0 z-50 opacity-60'>
+    <div className='fixed top-0 left-0 right-0 z-50 opacity-70'>
        <nav className="shadow-lg bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0">
+              <Link to={'/'}>
               <span className="text-black font-extrabold tracking-widest text-2xl ">Le Pamus </span>
+              </Link>
             </div>
           </div>
           <div className="hidden md:block ">
             <div className="ml-10 flex items-baseline space-x-4">
-              <a href="#" className="text-gray-900 hover:bg-gray-200  hover:font-bold px-3 py-2 rounded-md text-sm font-medium">Home</a>
-              <a href="#" className="text-gray-900 hover:bg-gray-200  hover:font-bold px-3 py-2 rounded-md text-sm font-medium">About</a>
-              <a href="#" className="text-gray-900 hover:bg-gray-200  hover:font-bold px-3 py-2 rounded-md text-sm font-medium">Services</a>
-              <a href="#" className="text-gray-900 hover:bg-gray-200  hover:font-bold px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+              <Link to={'/'} className="text-gray-900 hover:bg-gray-200  hover:font-bold px-3 py-2 rounded-md text-sm font-bold">Home</Link>
+              {/* <Link to={'/'} className="text-gray-900 hover:bg-gray-200  hover:font-bold px-3 py-2 rounded-md text-sm font-bold">About</Link> */}
+              <Link to={'/studentprofile'} className="text-gray-900 hover:bg-gray-200  hover:font-bold px-3 py-2 rounded-md text-sm font-bold">Student</Link>
+              <Link to={'/booking'} className="text-[orange] hover:bg-gray-200 opacity-100  hover:font-bold px-3 py-2 rounded-full hover:border text-sm font-bold">Book Now</Link>
+              
             </div>
           </div>
           <div className="md:hidden">
-            <button onClick={toggleNavbar} className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white">
+            <button onClick={toggleNavbar} className="inline-flex items-center justify-center p-2 rounded-md text-black hover:font-bold hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-black">
              <AiOutlineMenu/>
             </button>
           </div>
         </div>
       </div>
       {isOpen && (
-        <div className="md:hidden z-50">
+        <div className="md:hidden z-50 bg-white">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <a href="#" className="text-gray-900 hover:bg-gray-200 hover:font-bold block px-3 py-2 rounded-md text-base font-medium">Home</a>
-            <a href="#" className="text-gray-900 hover:bg-gray-200 hover:font-bold block px-3 py-2 rounded-md text-base font-medium">About</a>
-            <a href="#" className="text-gray-900 hover:bg-gray-200 hover:font-bold block px-3 py-2 rounded-md text-base font-medium">Services</a>
-            <a href="#" className="text-gray-900 hover:bg-gray-200 hover:font-bold block px-3 py-2 rounded-md text-base font-medium">Contact</a>
+            <Link to={'/'} className="text-gray-900 hover:bg-gray-200 hover:font-bold block px-3 py-2 rounded-md text-base font-medium">Home</Link>
+            {/* <Link to={'/'} className="text-gray-900 hover:bg-gray-200 hover:font-bold block px-3 py-2 rounded-md text-base font-medium">About</Link> */}
+            <Link to={'/studentprofile'} className="text-gray-900 hover:bg-gray-200 hover:font-bold block px-3 py-2 rounded-md text-base font-medium">Student</Link>
+            <Link to={'/booking'} className="text-gray-900 hover:bg-gray-200 hover:font-bold block px-3 py-2 rounded-md text-base font-medium">Book Now</Link>
           </div>
         </div>
       )}
--- src/client/Pages/Home.jsx
+++ src/client/Pages/Home.jsx
@@ -7,21 +7,20 @@ const Home = () => {
   return (
     <div>
      <Navbar/>
-      <div> 
-     <Carousel autoplay='true' loop='true'    className="rounded-sm overflow-x-hidden">
-     
-       <div className="relative h-[400] md:h-[550px]">
+      <div > 
+     <Carousel autoplay loop   className="rounded-sm overflow-x-hidden">
+       <div className="relative h-[400] md:h-[550px] md:mt-0 mt-16  ">
          <img
            src="https://images.unsplash.com/photo-1648301033733-44554c74ec50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
            alt="image 2"
            className="h-[400] w-full md:h-[550px] object-cover"
          />
          <div className="absolute inset-0 grid h-[400] md:h-[550px] items-center bg-black/75">
-           <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
+           <div className="w-3/4  ml-2 md:w-2/4 md:pl-20 lg:pl-32">
              <Typography
                variant="h1"
                color="white"
-               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+               className="mb-4 text-3xl md:text-4xl lg:text-5xl text-[orange] font-extrabold font-sans"
              >
                Le Pamus Residency
              </Typography>
@@ -30,35 +29,35 @@ const Home = () => {
                color="white"
                className="mb-12 opacity-80"
              >
-             Affordable Hostel facilites for Students
+             Affordable Hostel facility for Students
              </Typography>
              <div className="flex gap-2">
              <Link to={'/booking'}>
-               <Button size="lg" color="white">
+               <Button  className='px-7 py-4 ' color="white">
                  Book now
                </Button>
                </Link>
                <Link to={'/contactus'}>
-               <Button size="lg" className='flex gap-1 opacity-80 items-center font-bold' color="white" variant="lead">
-                 <span><AiOutlinePhone className='text-[brown] text-xl'/></span>  <span>Call/Contact Us</span>
+               <Button  className='flex gap-1 opacity-90 px-7 py-4 items-center font-bold' color="white" variant="lead">
+                 <span><AiOutlinePhone className='text-[brown] text-lg'/></span>  <span>Call/Contact Us</span>
                </Button>
                </Link>
              </div>
            </div>
          </div>
        </div>
-       <div className="relative h-[400] md:h-[550px]">
+       <div className="relative h-[400] md:h-[550px] md:mt-0 mt-16  ">
          <img
            src="https://images.unsplash.com/photo-1555854877-bab0e564b8d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80"
            alt="image 2"
            className="h-[400] w-full md:h-[550px] object-cover"
          />
          <div className="absolute inset-0 grid h-[400] md:h-[550px] items-center bg-black/75">
-           <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
+           <div className="w-3/4  ml-2 md:w-2/4 md:pl-20 lg:pl-32">
              <Typography
                variant="h1"
                color="white"
-               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+               className="mb-4 text-3xl md:text-4xl lg:text-5xl text-[orange] font-extrabold font-sans"
              >
                Le Pamus Residency
              </Typography>
@@ -67,166 +66,128 @@ const Home = () => {
                color="white"
                className="mb-12 opacity-80"
              >
-             Affordable Hostel facilites for Students
+             Affordable Hostel facility for Students
              </Typography>
              <div className="flex gap-2">
              <Link to={'/booking'}>
-               <Button size="lg" color="white">
+               <Button  className='px-7 py-4 ' color="white">
                  Book now
                </Button>
                </Link>
                <Link to={'/contactus'}>
-               <Button size="lg" className='flex gap-1 opacity-80 items-center font-bold' color="white" variant="lead">
-                 <span><AiOutlinePhone className='text-[brown] text-xl'/></span>  <span>Call/Contact Us</span>
+               <Button  className='flex gap-1 opacity-90 px-7 py-4 items-center font-bold' color="white" variant="lead">
+                 <span><AiOutlinePhone className='text-[brown] text-lg'/></span>  <span>Call/Contact Us</span>
                </Button>
                </Link>
              </div>
            </div>
          </div>
        </div>
-       
-       <div className="relative h-[400] md:h-[550px]">
+       <div className="relative h-[400] md:h-[550px] md:mt-0 mt-16  ">
          <img
            src="https://images.unsplash.com/photo-1519973759984-cf5a6c557cd8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80"
-           alt="image 3"
+           alt="image 2"
            className="h-[400] w-full md:h-[550px] object-cover"
          />
-         <div className="absolute inset-0 grid h-[400] md:h-[550px] items-end bg-black/75">
-           <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
+         <div className="absolute inset-0 grid h-[400] md:h-[550px] items-center bg-black/75">
+           <div className="w-3/4  ml-2 md:w-2/4 md:pl-20 lg:pl-32">
              <Typography
                variant="h1"
                color="white"
-               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+               className="mb-4 text-3xl md:text-4xl lg:text-5xl text-[orange] font-extrabold font-sans"
              >
-             Le Pamus Residency
+               Le Pamus Residency
              </Typography>
              <Typography
                variant="lead"
                color="white"
                className="mb-12 opacity-80"
              >
-             Affordable Hostel facilites for Students
+             Affordable Hostel facility for Students
              </Typography>
              <div className="flex gap-2">
              <Link to={'/booking'}>
-               <Button size="lg" color="white">
+               <Button  className='px-7 py-4 ' color="white">
                  Book now
                </Button>
                </Link>
                <Link to={'/contactus'}>
-               <Button size="lg" className='flex gap-1 opacity-80 items-center font-bold' color="white" variant="lead">
-                 <span><AiOutlinePhone className='text-[brown] text-xl'/></span>  <span>Call/Contact Us</span>
+               <Button  className='flex gap-1 opacity-90 px-7 py-4 items-center font-bold' color="white" variant="lead">
+                 <span><AiOutlinePhone className='text-[brown] text-lg'/></span>  <span>Call/Contact Us</span>
                </Button>
                </Link>
              </div>
            </div>
          </div>
        </div>
-       <div className="relative h-[400] md:h-[550px]">
+       <div className="relative h-[400] md:h-[550px] md:mt-0 mt-16  ">
          <img
-           src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-           alt="image 3"
+           src="https://images.unsplash.com/photo-1460518451285-97b6aa326961?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+           alt="image 2"
            className="h-[400] w-full md:h-[550px] object-cover"
          />
-         <div className="absolute inset-0 grid h-[400] md:h-[550px] items-end bg-black/75">
-           <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
+         <div className="absolute inset-0 grid h-[400] md:h-[550px] items-center bg-black/75">
+           <div className="w-3/4  ml-2 md:w-2/4 md:pl-20 lg:pl-32">
              <Typography
                variant="h1"
                color="white"
-               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+               className="mb-4 text-3xl md:text-4xl lg:text-5xl text-[orange] font-extrabold font-sans"
              >
-             Le Pamus Residency
+               Le Pamus Residency
              </Typography>
              <Typography
                variant="lead"
                color="white"
                className="mb-12 opacity-80"
              >
-             Affordable Hostel facilites for Students
+             Affordable Hostel facility for Students
              </Typography>
              <div className="flex gap-2">
              <Link to={'/booking'}>
-               <Button size="lg" color="white">
+               <Button  className='px-7 py-4 ' color="white">
                  Book now
                </Button>
                </Link>
                <Link to={'/contactus'}>
-               <Button size="lg" className='flex gap-1 opacity-80 items-center font-bold' color="white" variant="lead">
-                 <span><AiOutlinePhone className='text-[brown] text-xl'/></span>  <span>Call/Contact Us</span>
+               <Button  className='flex gap-1 opacity-90 px-7 py-4 items-center font-bold' color="white" variant="lead">
+                 <span><AiOutlinePhone className='text-[brown] text-lg'/></span>  <span>Call/Contact Us</span>
                </Button>
                </Link>
              </div>
            </div>
          </div>
        </div>
-       <div className="relative h-[400] md:h-[550px]">
-         <img
-           src="https://images.unsplash.com/photo-1507537509458-b8312d35a233?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
-           alt="image 3"
-           className="h-[400] w-full md:h-[550px] object-cover"
-         />
-         <div className="absolute inset-0 grid h-[400] md:h-[550px] items-end bg-black/75">
-           <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
-             <Typography
-               variant="h1"
-               color="white"
-               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
-             >
-             Le Pamus Residency
-             </Typography>
-             <Typography
-               variant="lead"
-               color="white"
-               className="mb-12 opacity-80"
-             >
-             Affordable Hostel facilites for Students
-             </Typography>
-             <div className="flex gap-2">
-             <Link to={'/booking'}>
-               <Button size="lg" color="white">
-                 Book now
-               </Button>
-               </Link>
-               <Link to={'/contactus'}>
-               <Button size="lg" className='flex gap-1 opacity-80 items-center font-bold' color="white" variant="lead">
-                 <span><AiOutlinePhone className='text-[brown] text-xl'/></span>  <span>Call/Contact Us</span>
-               </Button>
-               </Link>
-             </div>
-           </div>
-         </div>
-       </div>
-       <div className="relative h-[400] md:h-[550px]">
+       <div className="relative h-[400] md:h-[550px] md:mt-0 mt-16  ">
          <img
            src="https://images.unsplash.com/photo-1427504494785-3a9ca7044f45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
-           alt="image 3"
+           alt="image 2"
            className="h-[400] w-full md:h-[550px] object-cover"
          />
-         <div className="absolute inset-0 grid h-[400] md:h-[550px] items-end bg-black/75">
-           <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
+         <div className="absolute inset-0 grid h-[400] md:h-[550px] items-center bg-black/75">
+           <div className="w-3/4  ml-2 md:w-2/4 md:pl-20 lg:pl-32">
              <Typography
                variant="h1"
                color="white"
-               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+               className="mb-4 text-3xl md:text-4xl lg:text-5xl text-[orange] font-extrabold font-sans"
              >
-             Le Pamus Residency
+               Le Pamus Residency
              </Typography>
              <Typography
                variant="lead"
                color="white"
                className="mb-12 opacity-80"
              >
-             Affordable Hostel facilites for Students
+             Affordable Hostel facility for Students
              </Typography>
              <div className="flex gap-2">
              <Link to={'/booking'}>
-               <Button size="lg" color="white">
+               <Button  className='px-7 py-4 ' color="white">
                  Book now
                </Button>
                </Link>
                <Link to={'/contactus'}>
-               <Button size="lg" className='flex gap-1 opacity-80 items-center font-bold' color="white" variant="lead">
-                 <span><AiOutlinePhone className='text-[brown] text-xl'/></span>  <span>Call/Contact Us</span>
+               <Button  className='flex gap-1 opacity-90 px-7 py-4 items-center font-bold' color="white" variant="lead">
+                 <span><AiOutlinePhone className='text-[brown] text-lg'/></span>  <span>Call/Contact Us</span>
                </Button>
                </Link>
              </div>
